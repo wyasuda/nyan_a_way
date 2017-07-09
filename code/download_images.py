4a2e32625a1380e258314ae6c5e323744546976c
wyasuda <wataru.robo@gmail.com>
2017-07-09 13:29:14 +0900

    Update download_images.py

--- code/download_images.py
+++ code/download_images.py
@@ -9,9 +9,9 @@ all_tgt = ["cat", "dog", "person", "yard"]
 
 for cur_tgt in all_tgt:
     print 'Category : ' + str(cur_tgt) + '\n\n'
-    f1 = open('/home/ubuntu/OpenImagesDataset/machine_ann_2016_08/train/labels_'+str(cur_tgt)+'.csv', 'r')
+    f1 = open('/home/ubuntu/labels_'+str(cur_tgt)+'.csv', 'r')
     reader1 = csv.reader(f1)
-    f2 = open('/home/ubuntu/OpenImagesDataset/images_2016_08/train/images.csv', 'r')
+    f2 = open('/home/ubuntu/images_2016_08/train/images.csv', 'r')
     reader2 = csv.reader(f2)
     confidence = 1.0
     if cur_tgt == 'yard':
@@ -21,19 +21,19 @@ for cur_tgt in all_tgt:
     for row1 in reader1:
 
         if float(row1[3]) == confidence:
-            
+
             for row2 in reader2:
                 if row1[0] == row2[0]:
                     break
             print str(cnt)+' : '+row2[2]
-            
+
             #load image
             urlimg = urllib.urlopen(row2[2])
             try:
                 img = Image.open(urlimg)
             except:
                 continue # skip if error happened
-            
+
             fname = '/home/ubuntu/downloaded_images/'+str(cur_tgt)+'.'+str(cnt)+'.jpg'
             if urlimg.url.find('unavailable') == -1:
                 if cur_tgt != 'yard':
@@ -45,7 +45,7 @@ for cur_tgt in all_tgt:
                     img_resize.save(fname)
                 else:
                     img.save(fname)
-                
+
                 cnt = cnt + 1
                 if 10000 <= cnt:
                     break

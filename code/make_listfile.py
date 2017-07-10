37cc70706c3f709d83a685da90aa4e887835c45d
wyasuda <wataru.robo@gmail.com>
2017-07-10 15:38:16 +0900

    Update make_listfile.py

--- code/make_listfile.py
+++ code/make_listfile.py
@@ -5,7 +5,7 @@ f = open('~/list_train.txt', 'w')
 gc = 0
 for grp in gr:
     for i in range(9000):
-        tmpstr = "images/train/" + str(grp) + "." + str(i) + ".jpg " + str(gc) + "\n"
+        tmpstr = str(grp) + "." + str(i) + ".jpg " + str(gc) + "\n"
         f.write(tmpstr)
     gc = gc + 1
 f.close()
@@ -14,7 +14,7 @@ f = open('~/list_test.txt', 'w')
 gc = 0
 for grp in gr:
     for i in range(9000, 10000):
-        tmpstr = "images/test/" + str(grp) + "." + str(i) + ".jpg " + str(gc) + "\n"
+        tmpstr = str(grp) + "." + str(i) + ".jpg " + str(gc) + "\n"
         f.write(tmpstr)
     gc = gc + 1
 f.close()
